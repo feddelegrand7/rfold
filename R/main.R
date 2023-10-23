cdad5e9d4e7bf7889824214e7a37fd23eb197565
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2023-10-23 12:33:08 +0200

    adding the folders_to_ignore parameter

--- R/main.R
+++ R/main.R
@@ -1,5 +1,5 @@
 
-.get_files_that_are_not_in_r_folder <- function() {
+.get_files_that_are_not_in_r_folder <- function(folders_to_ignore) {
 
   r_folder_exists <- unname(fs::dir_exists(here::here("R")))
 
@@ -36,7 +36,9 @@
     )
   }
 
-  r_files_not_in_r_dir_df <- tmp_df[tmp_df$dir_name != "R", ]
+  folders_to_be_ignored <- c("R", folders_to_ignore)
+
+  r_files_not_in_r_dir_df <- tmp_df[!tmp_df$dir_name %in% folders_to_be_ignored, ]
 
   r_files_not_in_r_dir <- r_files_not_in_r_dir_df$r_files
 
@@ -69,12 +71,21 @@
 #'
 #' @return called for the side effect of transferring all R files
 # available inside a project into the R folder
+#' @param folders_to_ignore a string vector of plain names folders that should be ignored
+#' where transferring .R files into the main R folder (for example a dev folder). Defaults to NULL
+#'
 #' @export
 #'
 
-fold <- function() {
+fold <- function(folders_to_ignore = NULL) {
 
-  r_files <- .get_files_that_are_not_in_r_folder()
+  if (!is.null(folders_to_ignore)) {
+    if (any(grepl("/", folders_to_ignore))) {
+      stop("Please provide plain folder names (without the backslash /)")
+    }
+  }
+
+  r_files <- .get_files_that_are_not_in_r_folder(folders_to_ignore)
 
   if (length(r_files) == 0) {
     return(invisible(NULL))
